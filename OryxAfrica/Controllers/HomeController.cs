a76bb4704595f8d059ae666a0c20ca2319b5e249
Adeleke.Amoo <Adeleke.Amoo@ADELEKEAMOO>
2018-07-31 14:22:12 +0100

    k

--- OryxAfrica/Controllers/HomeController.cs
+++ OryxAfrica/Controllers/HomeController.cs
@@ -17,11 +17,6 @@ namespace OryxWeb.Controllers
 
         public IActionResult About()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
             ViewData["Message"] = "Your application description page.";
 
             return View();
